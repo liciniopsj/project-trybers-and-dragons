d732ed12581ebbf18c8c58f06383262704677435
Licínio Pereira dos Santos Junior <licinio.psj@gmail.com>
2023-05-08 18:12:20 -0300

    feat: Requisite 02

---
+++ src/Races/Halfling.ts
@@ -0,0 +1,20 @@
+import Race from './Race';
+
+export default class Halfling extends Race {
+  private static instances = 0;
+
+  constructor(
+    name: string,
+    dexterity: number,
+    private _maxLifePoints: number = 60,
+  ) { super(name, dexterity); }
+
+  get maxLifePoints(): number {
+    return this._maxLifePoints;
+  }
+
+  static createdRacesInstances(): number {
+    this.instances += 1;
+    return this.instances;
+  }
+}
